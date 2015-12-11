27c0250ee8d01110a7631282ea654c389f7e8b16
ジョセフ·ダミアン·ガリード <damian.garrido.munoz@gmail.com>
2015-12-11 15:52:22 -0300

    small fix
    * change repo
    * display in recent news order

--- js/prensa.js
+++ js/prensa.js
@@ -4,7 +4,7 @@ var app = angular.module('prensaApp', ['ngRoute','angular.filter'], function($in
 });
 
 app.controller('newsController', ["$scope", "$http", "$timeout", function ($scope, $http, $timeout){
-  get_news = "//api.morph.io/jdgarrido/prensa-spreadsheet-storage/data.json?key=C317BJoPzKOOMj%2B83VbD&query=select%20*%20from%20%27data%27%20order%20by%20fecha%20desc&callback=JSON_CALLBACK"
+  get_news = "//api.morph.io/ciudadanointeligente/prensa-ci/data.json?key=C317BJoPzKOOMj%2B83VbD&query=select%20*%20from%20%27data%27%20order%20by%20fecha%20desc&callback=JSON_CALLBACK"
   $http.jsonp(get_news)
       .then( function (response){
         $scope.allnews = response.data
@@ -13,6 +13,9 @@ app.controller('newsController', ["$scope", "$http", "$timeout", function ($scop
         var icon_class = {'radio':'fa fa-microphone', 'radio-online':'fa fa-microphone', 'impreso':'fa fa-newspaper-o', 'impreso-online':'fa fa-newspaper-o','tv':'fa fa-television', 'tv-online':'fa fa-television','online':'fa fa-globe' }
         for (i in $scope.allnews){
           var news = $scope.allnews[i]
+          tmp_date = $scope.allnews[i]['fecha'].split('/')
+          unixtime = new Date(tmp_date[2]+'/'+tmp_date[1]+'/'+tmp_date[0])
+          news.unixtime = unixtime.getTime()
         	news.color = color[news.tipo_prensa]
           news.icon_class = icon_class[news.tipo_prensa]
         }
