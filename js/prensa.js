13c7226c5e572488a08970a997296cb4b03b1c3e
damian-garrido <damian.garrido.munoz@gmail.com>
2017-03-20 16:38:53 -0300

    update

--- js/prensa.js
+++ js/prensa.js
@@ -4,22 +4,26 @@ var app = angular.module('prensaApp', ['ngRoute','angular.filter'], function($in
 });
 
 app.controller('newsController', ["$scope", "$http", "$timeout", function ($scope, $http, $timeout){
-  get_news = "//api.morph.io/ciudadanointeligente/prensa-ci/data.json?key=C317BJoPzKOOMj%2B83VbD&query=select%20*%20from%20%27data%27%20order%20by%20fecha%20desc&callback=JSON_CALLBACK"
-  $http.jsonp(get_news)
-      .then( function (response){
-        $scope.allnews = response.data
-        var i;
-        var color = {'radio':'pink', 'radio-online':'pink', 'impreso':'calypso', 'impreso-online':'calypso','tv':'orange', 'tv-online':'orange','online':'purple' }
-        var icon_class = {'radio':'fa fa-microphone', 'radio-online':'fa fa-microphone', 'impreso':'fa fa-newspaper-o', 'impreso-online':'fa fa-newspaper-o','tv':'fa fa-television', 'tv-online':'fa fa-television','online':'fa fa-globe' }
-        for (i in $scope.allnews){
+  get_news = "//api.morph.io/ciudadanointeligente/prensa-ci/data.json?key=NB5JgFSb%2FeF87jzVs983&query=select%20*%20from%20'data'%20where%20destacada%20%3D%20%221%22%20order%20by%20fecha%20DESC&callback=JSON_CALLBACK"
+
+    $http.jsonp(get_news)
+    .then( function (response){
+      $scope.allnews = response.data
+      var i;
+      var color = {'radio':'pink', 'radio-online':'pink', 'impreso':'calypso', 'impreso-online':'calypso','tv':'orange', 'tv-online':'orange','online':'purple' }
+      var icon_class = {'radio':'fa fa-microphone', 'radio-online':'fa fa-microphone', 'impreso':'fa fa-newspaper-o', 'impreso-online':'fa fa-newspaper-o','tv':'fa fa-television', 'tv-online':'fa fa-television','online':'fa fa-globe' }
+      var months = ["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"]
+      for (i in $scope.allnews){
+        if ($scope.allnews[i]['destacada'] === "1") {
           var news = $scope.allnews[i]
-          tmp_date = $scope.allnews[i]['fecha'].split('/')
-          unixtime = new Date(tmp_date[2]+'/'+tmp_date[1]+'/'+tmp_date[0])
-          news.unixtime = unixtime.getTime()
-        	news.color = color[news.tipo_prensa]
+          news.unixtime = $scope.allnews[i]['fecha']
+          n_date = new Date(parseInt($scope.allnews[i]['fecha'])).getDate().toString() +'-'+ months[new Date(parseInt($scope.allnews[i]['fecha'])).getMonth()].toString() +'-'+ new Date(parseInt($scope.allnews[i]['fecha'])).getFullYear().toString() //#
+          news.format_date = n_date
+          news.color = color[news.tipo_prensa.toLowerCase()]
           news.icon_class = icon_class[news.tipo_prensa]
         }
-      }, function (response){
-        console.log(response)
-      })
+      }
+    }, function (response){
+      console.log(response)
+    })
 }]);
